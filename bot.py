961e1a4d85b733e1c2b00a0eb5e9d833dee1310e
browner123 <34919919+browner123@users.noreply.github.com>
2019-02-17 15:53:40 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import random
+import os
+
+
+Client = discord.Client() 
+client = commands.Bot(command_prefix = "?") 
+
+
+@client.event 
+async def on_ready():
+    print("Bot is online and connected to Discord") 
+
+@client.event
+async def on_message(message):
+    if message.content == "Hey":
+        time.sleep(2)
+        await client.send_message(message.channel, random.choice(["Whats up guys", "Hello", "Hey", "What is going on"])) 
+    
+client.run(os.getenv('TOKEN'))
